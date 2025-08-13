3c59b1357fe7f960a9459140576115461993df70
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-08-13 15:32:53 +0200

    feat: add button component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,6 @@
+// src/app/page.tsx
+import { redirect } from 'next/navigation';
 
-
-export default function Home() {
-  return (
-   <div className="text-center">
-     <h1 className="text-4xl font-bold mt-10">Home Page</h1>
-   </div>
-  );
+export default function RootRedirect() {
+  redirect('/en'); // sau '/it'
 }
